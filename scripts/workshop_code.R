6c55b57f1b1fb31094522e7b88a75d3ba967e60f
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-09-12 14:58:37 -0400

    'update'

--- scripts/workshop_code.R
+++ scripts/workshop_code.R
@@ -6,3 +6,18 @@ library(dplyr)
 
 # Read in data
 dc_metro <- st_read(here("data/Metro_Lines.shp"))
+dc_metro_sttn <- st_read(here("data/metrostations.geojson"))
+us_states <- st_read(here("data/tl_2015_us_state.shp"))
+dc_elev <- raster(here("data/dc_ned.tif"))
+dc_nlcd <- raster(here("data/dc_nlcd.tif"))
+
+
+# Write out data
+st_write(dc_metro, here("data/dc_metro.geojson"))
+
+# Using dplyr with sf
+dc_red_only <- dc_metro %>%
+  filter(NAME == "red")
+
+
+
