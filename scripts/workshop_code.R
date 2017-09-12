130698062dd3a748b5201dacea6a12252080c570
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-09-12 15:43:06 -0400

    'update'

--- scripts/workshop_code.R
+++ scripts/workshop_code.R
@@ -34,3 +34,9 @@ utm_z18 <- "+proj=utm +zone=18 +ellps=WGS84 +datum=WGS84 +units=m +no_defs "
 dc_metro_alb <- st_transform(dc_metro, esri_alb_p4)
 dc_elev_alb <- projectRaster(dc_elev,crs = esri_alb_p4)
 
+dc_bnd_alb <- us_states %>%
+  filter(STUSPS == "DC") %>%
+  st_transform(esri_alb_p4)
+
+
+
